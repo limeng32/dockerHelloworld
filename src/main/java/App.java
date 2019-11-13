53cce8b72bb03dbd5ea23999cc2840fb79efc1c8
limeng <limeng32@live.cn>
2019-11-13 16:15:00 +0800

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,6 @@
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello, multi-stage build...Cool!");
+		System.out.println("Hello, multi-stage build...Cool !");
 	}
 }
